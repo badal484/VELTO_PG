3ea01c5867a64e408b73ee43c78a8c3adee74195
badal484 <badal90603@gmail.com>
2026-04-25 10:54:57 +0530

    createNotification.js

---
+++ backend/src/utils/createNotification.js
@@ -0,0 +1,22 @@
+const Notification = require('../models/Notification');
+
+const createNotification = async (userId, type, title, message, link = '/') => {
+  try {
+    if (!userId) return; // Cannot notify if no user ID
+    const notification = await Notification.create({
+      user: userId,
+      type,
+      title,
+      message,
+      link,
+    });
+
+    if (global.io && userId) {
+      global.io.to(userId.toString()).emit('notification', notification);
+    }
+  } catch (err) {
+    console.error('Notification creation failed:', err.message);
+  }
+};
+
+module.exports = createNotification;
