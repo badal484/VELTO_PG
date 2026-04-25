6736a428edcf93229ee100d4b0a4b9e51a7f0fb8
badal484 <badal90603@gmail.com>
2026-04-25 10:54:26 +0530

    calculateRefund.js

---
+++ backend/src/utils/calculateRefund.js
@@ -0,0 +1,28 @@
+const calculateRefund = (totalPrice, checkInDate) => {
+  const daysUntilCheckIn = Math.ceil((new Date(checkInDate) - new Date()) / (1000 * 60 * 60 * 24));
+  let refundPercent = 0, message = '', timeline = '';
+
+  if (daysUntilCheckIn > 7) {
+    refundPercent = 90;
+    message = 'Full refund (90%)';
+    timeline = 'Cancelled more than 7 days before check-in';
+  } else if (daysUntilCheckIn >= 3) {
+    refundPercent = 50;
+    message = 'Partial refund (50%)';
+    timeline = 'Cancelled 3–7 days before check-in';
+  } else {
+    refundPercent = 0;
+    message = 'No refund';
+    timeline = 'Cancelled less than 3 days before check-in';
+  }
+
+  return {
+    refundPercent,
+    refundAmount: Math.round((totalPrice * refundPercent) / 100),
+    daysUntilCheckIn,
+    message,
+    timeline,
+  };
+};
+
+module.exports = calculateRefund;
